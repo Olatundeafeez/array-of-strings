b65e93097842e261bc994851aebe84897310addf
Olatundeafeez <olatundefolorunshoafeez@gmail.com>
2024-05-26 22:19:02 +0100

    new commit

---
+++ looping the bulb/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = { "baba", "fortunate", "deolu", "shakirat" };
+            
+            foreach( string name in names)
+
+            {
+                
+                Console.WriteLine(name[name.Length-1].ToString().ToUpper());
+            }
+        }
+    }
+}
